963b6234515df5318dbb0a39ea581e6e3f73d7da
WoohyunSHIN <swh159@gmail.com>
2021-09-23 14:44:23 +0200

    create a new file

---
+++ Programmers/Weekly_Challenge/insufficient_price.py
@@ -0,0 +1,20 @@
+from sys import stdin
+"""
+3
+20
+4
+"""
+price = int(stdin.readline().rstrip())
+money = int(stdin.readline().rstrip())
+count = int(stdin.readline().rstrip())
+
+def solution(price:int, money:int, count:int)->int:
+    answer = -1
+    cnt_sum = sum(range(1,count+1))
+    if money <= price * cnt_sum:
+        answer =  (price * cnt_sum) - money
+    else:
+        answer = -1    
+    return answer
+
+print(solution(price,money,count))
